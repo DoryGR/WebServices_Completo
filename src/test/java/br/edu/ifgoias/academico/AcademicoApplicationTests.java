fe58062bfcb7baa631783a9eb6cd4e37a000e92f
Henrique de Azara Tosta <81333232+HenriqueAzT@users.noreply.github.com>
2023-11-24 14:41:31 -0300

    Update AcademicoApplicationTests.java

--- src/test/java/br/edu/ifgoias/academico/AcademicoApplicationTests.java
+++ src/test/java/br/edu/ifgoias/academico/AcademicoApplicationTests.java
@@ -1,4 +1,4 @@
-package br.edu.ifgoias.academico;
+/*package br.edu.ifgoias.academico;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,3 +11,4 @@ class AcademicoApplicationTests {
 	}
 
 }
+*/
