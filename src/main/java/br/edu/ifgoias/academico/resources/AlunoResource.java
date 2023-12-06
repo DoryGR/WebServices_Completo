767325ef85c70e244155c37eed7d21890ee64e0f
Henrique de Azara Tosta <81333232+HenriqueAzT@users.noreply.github.com>
2023-12-06 11:39:11 -0300

    Update AlunoResource.java

--- src/main/java/br/edu/ifgoias/academico/resources/AlunoResource.java
+++ src/main/java/br/edu/ifgoias/academico/resources/AlunoResource.java
@@ -1,7 +1,5 @@
 package br.edu.ifgoias.academico.resources;
-
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,57 +13,40 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
 import br.edu.ifgoias.academico.entities.Aluno;
 import br.edu.ifgoias.academico.services.AlunoService;
-
 @RestController
 @CrossOrigin("http://localhost:4200")
 @RequestMapping(value = "/alunos")
-
 public class AlunoResource {
-
 	@Autowired
     private AlunoService alunoService;
-
 	@GetMapping
 	public ResponseEntity<List<Aluno>> findAll() {
-
 		List<Aluno> alunos = alunoService.findAll();
-
 		return ResponseEntity.ok().body(alunos);
-
 	}
-
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Aluno> findById(@PathVariable Integer id) {
-
 		Aluno aluno = alunoService.findById(id);
-
 		return ResponseEntity.ok().body(aluno);
 	}
-
 	@PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<Aluno> insert(@RequestBody Aluno aluno) {
         Aluno createdAluno = alunoService.insert(aluno);
         return new ResponseEntity<>(createdAluno, HttpStatus.CREATED);
     }
-
 	@DeleteMapping(value = "/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		alunoService.delete(id);
 		return ResponseEntity.noContent().build();
 	}
-
-
 	@PutMapping(value = "/{id}")
 	@ResponseStatus(HttpStatus.OK)
 	public ResponseEntity<Aluno> update(@PathVariable Integer id, @RequestBody Aluno a) {
-
 		a = alunoService.update(id, a);
-
 		return ResponseEntity.ok().body(a);
 	}
 }
